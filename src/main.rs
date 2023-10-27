f4c827fb9892fef19bd305d80069c863a15a3950
abhi3700 <dalvath3700@gmail.com>
2023-10-27 13:38:37 +0530

    added features
    - connected to RPC URL
    - generate keypairs based on the account_num provided & also collect the addresses for funding them from funder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ethers::{core::rand::thread_rng, prelude::*, signers::LocalWallet, utils::hex};
+use eyre::Result;
 use std::str::FromStr;
 use structopt::StructOpt;
 
@@ -24,7 +26,7 @@ struct Cli {
     #[structopt(short = "f", long)]
     funding_amount: u64,
 
-    /// Ethereum RPC node URL
+    /// Subspace EVM (Nova) RPC node URL
     #[structopt(short = "r", long)]
     rpc_url: String,
 }
@@ -47,16 +49,39 @@ impl FromStr for TransactionType {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opt = Cli::from_args();
 
     match opt.transaction_type.parse::<TransactionType>() {
         Ok(transaction_type) => match opt.num_blocks {
             Some(num_blocks) => {
-                println!("Running {} transaction(s) of type {:?} for {} block(s) with {} account(s) and funding amount of {} wei using initial funded account with private key {} and Ethereum RPC node URL {}", 
-                    opt.num_accounts, transaction_type, num_blocks, opt.num_accounts, opt.funding_amount, opt.initial_funded_account_private_key, opt.rpc_url);
+                // println!("Running {} transaction(s) of type {:?} for {} block(s) with {} account(s) and funding amount of {} wei using initial funded account with private key {} and Ethereum RPC node URL {}", opt.num_accounts, transaction_type, num_blocks, opt.num_accounts, opt.funding_amount, opt.initial_funded_account_private_key, opt.rpc_url);
+
+                // get a client connected to parsed RPC node URL
+                let client: Provider<Http> = Provider::<Http>::try_from(opt.rpc_url)
+                    .expect("Failed to connect! Please provide a valid RPC URL");
+
+                let mut wallet_addresses = Vec::<Address>::new();
+                let mut wallet_priv_keys = Vec::<String>::new();
+                // generate multiple accounts based on the parsed number.
+                for _ in 0..opt.num_accounts {
+                    let mut rng = thread_rng();
+                    let wallet = LocalWallet::new(&mut rng);
+                    // println!("Successfully created new keypair.");
+                    // println!("Address:     {:?}", wallet.address());
+                    // println!("Private key: 0x{}", hex::encode(wallet.signer().to_bytes()));
+                    wallet_addresses.push(wallet.address());
+                    wallet_priv_keys.push(format!("0x{}", hex::encode(wallet.signer().to_bytes())));
+                }
+
+                // TODO: transfer fund to each account from the set funder
+                // if funder has non-zero balance
+
+                // else throw an error
             }
             None => {
+                // TODO: We can throw error here.
                 println!("Running {} transaction(s) of type {:?} indefinitely with {} account(s) and funding amount of {} wei using initial funded account with private key {} and Ethereum RPC node URL {}", 
                     opt.num_accounts, transaction_type, opt.num_accounts, opt.funding_amount, opt.initial_funded_account_private_key, opt.rpc_url);
             }
@@ -65,4 +90,6 @@ fn main() {
             println!("{}", e);
         }
     }
+
+    Ok(())
 }
