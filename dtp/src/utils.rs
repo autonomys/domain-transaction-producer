08677fca92a171b9fec07cd8d1fa5b95888c2a09
abhi3700 <dalvath3700@gmail.com>
2023-11-04 00:21:49 +0530

    Added batch handling for async calls in Load contract's setArray method

--- dtp/src/utils.rs
+++ dtp/src/utils.rs
@@ -1,4 +1,4 @@
-use bindings::{counter::Counter, fund::Fund};
+use bindings::{counter::Counter, fund::Fund, load::Load};
 use ethers::{
     core::{k256::ecdsa::SigningKey, rand::thread_rng},
     prelude::*,
@@ -9,7 +9,16 @@ use ethers::{
 use futures::future::join_all;
 use std::sync::Arc;
 
-const MAX_BATCH_CHUNK_SIZE: u16 = 100;
+// max. no. of txs that can be sent in a batch unlike
+// sending all txs at once which is failing due
+// to too many connections at once.
+const MAX_BATCH_SIZE: u16 = 100;
+
+// max. value of `count` in `setArray` method of Load contract
+// which is allowed to be added in a block. So, with this value
+// set, we get the gas cost of (59.98 M) ~60 M per block. Foundry
+// tests done with numerous value in `Load.t.sol` file.
+const MAX_LOAD_COUNT_PER_BLOCK: u16 = 2650;
 
 /// Convert Wei to TSSC (in String)
 pub(crate) fn wei_to_tssc_string(bal_wei: U256) -> String {
@@ -225,15 +234,13 @@ pub(crate) async fn multicall_light_txs_1(
 /// Use `batch` to run each batch via `join_all(batch).await`. E.g. for 1000 connections,
 /// there would be 10 batches of 100 calls/requests each. Now, each batch i.e. 100 requests
 /// is sent at once, unlike all 1000 (total) calls sent at once as was done previously.
-async fn handle_async_calls_in_batch(
+async fn handle_async_calls_in_batch_light(
     client: Arc<Provider<Http>>,
     counter_address: Address,
     signers: Vec<Wallet<SigningKey>>,
 ) -> eyre::Result<()> {
-    // TODO: add chunk_size related code
-
-    // iteration in chunk of `MAX_BATCH_CHUNK_SIZE`
-    for chunk in signers.chunks(MAX_BATCH_CHUNK_SIZE.into()) {
+    // iteration in chunk of `MAX_BATCH_SIZE`
+    for chunk in signers.chunks(MAX_BATCH_SIZE.into()) {
         // create a batch vec for this chunk
         let mut batch = Vec::new();
 
@@ -260,8 +267,9 @@ pub(crate) async fn multicall_light_txs_2(
         .expect("Unable to get Counter number before calls.");
     println!("\nNumber stored in \'Counter\' before calls: {}", num_before);
 
-    // Handle async calls in batches where each batch has `MAX_BATCH_CHUNK_SIZE` requests.
-    handle_async_calls_in_batch(client.to_owned(), counter_address, signers.to_owned()).await?;
+    // Handle async calls in batches where each batch has `MAX_BATCH_SIZE` requests.
+    handle_async_calls_in_batch_light(client.to_owned(), counter_address, signers.to_owned())
+        .await?;
 
     // get the number value before calls
     let num_after = counter_get_number(client.clone(), counter_address)
@@ -272,15 +280,91 @@ pub(crate) async fn multicall_light_txs_2(
     Ok(())
 }
 
-/// As HEAVY transaction type, multicall particular function
-/// let's say `increment` successively done by each new accounts w/o
-/// `num_block` cli arg.
-pub(crate) async fn multicall_heavy_txs(
-    provider: &'static Provider<Http>,
+/// Load contract: `setArray` method
+/// NOTE: signer needed as it incurs gas fees.
+pub(crate) async fn load_set_array(
+    client: Arc<Provider<Http>>,
+    load_address: Address,
+    signer: Wallet<SigningKey>,
+) -> eyre::Result<()> {
+    // create a middleware client with signature from signer & provider
+    let client_middleware = SignerMiddleware::new(
+        client.clone(),
+        signer.with_chain_id(client.get_chainid().await?.as_u64()),
+    );
+
+    // clone the client (if multiple use)
+    let client_middleware = Arc::new(client_middleware);
+
+    // get a contract
+    let load = Load::new(load_address, client_middleware);
+
+    // TODO: Here, `count` can be abstracted out as CLI parameter with default value set as may be `1000`
+    // considered the highest possible count per block for now.
+    let count = MAX_LOAD_COUNT_PER_BLOCK;
+
+    // send a transaction with setter function
+    let tx_receipt = load
+        // to try out with its different values.
+        // The max. `count` possible in `setArray` method of Load contract is 2650. Above this count value,
+        // the gas cost exceeds 60 M per block (as set for Subspace EVM domain).
+        .set_array(U256::from(count))
+        .send()
+        .await
+        .expect("Failure in getting pending tx")
+        .await?
+        .expect("Failure in \'setArray\' method of Load contract");
+    println!(
+        "\n\'{}\' set Array with count {}, which incurred a gas fee of \'{} TSSC\', has a tx hash: \'{:?}\', indexed at #{} in block #{}.",
+        tx_receipt.from,
+        count,
+        get_gas_cost(&client.clone(), &tx_receipt).await?,
+        tx_receipt.transaction_hash,
+        tx_receipt.transaction_index,
+        tx_receipt.block_number.unwrap()
+    );
+
+    Ok(())
+}
+
+/// Like `handle_async_calls_in_batch_light` but for HEAVY txs.
+/// Considered `Load` contract's `setArray` method as HEAVY txs.
+async fn handle_async_calls_in_batch_heavy(
+    client: Arc<Provider<Http>>,
     load_address: Address,
     signers: Vec<Wallet<SigningKey>>,
-) {
-    // TODO:
+) -> eyre::Result<()> {
+    // iteration in chunk of `MAX_BATCH_SIZE`
+    for chunk in signers.chunks(MAX_BATCH_SIZE.into()) {
+        // create a batch vec for this chunk
+        let mut batch = Vec::new();
+
+        for signer in chunk {
+            batch.push(load_set_array(client.to_owned(), load_address, signer.to_owned()));
+        }
+
+        // send txs in batch of 100 (set now, can be adjusted later)
+        join_all(batch).await;
+    }
+
+    Ok(())
+}
+
+/// As HEAVY transaction type, multicall particular function of Load contract
+/// let's say `setArray` method successively done by each new accounts w/o
+/// `num_txs` cli arg. Here, instead of sending `calls` at once via `join_all(calls).await`,
+/// use `batch` to run each batch via `join_all(batch).await`. E.g. for 1000 connections,
+/// there would be 10 batches of 100 calls/requests each. Now, each batch i.e. 100 requests
+/// is sent at once, unlike all 1000 (total) calls sent at once as done similar for light txs.
+pub(crate) async fn multicall_heavy_txs_2(
+    client: Arc<Provider<Http>>,
+    load_address: Address,
+    signers: Vec<Wallet<SigningKey>>,
+) -> eyre::Result<()> {
+    // Handle async calls in batches where each batch has `MAX_BATCH_SIZE` requests.
+    handle_async_calls_in_batch_heavy(client.to_owned(), load_address, signers.to_owned()).await?;
+
+    Ok(())
 }
 
 /// Get contract addresses from env variables from `.env` file
